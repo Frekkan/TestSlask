633670ab57a9195df096ddbd9fdc1edbea13ab05
Frekkan <frekkan1111@hotmail.com>
2016-01-28 14:43:54 +0100

    Initial commit of SalesTools

---
+++ LocalSalesTools/SalesTools/src/ca/demo/salestools/Main.java
@@ -0,0 +1,19 @@
+package ca.demo.salestools;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SalesData data = new SalesData();
+		
+		displayGreetin();
+		data.display();
+		
+	}
+	
+	private static void displayGreetin() {
+		System.out.println("HELLO0 HAPPY SALES PEOPLE");
+		System.out.println("THIS APP SHOWS SALES DATA");
+	}
+
+}
